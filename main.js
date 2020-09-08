a7f2e4bcf75b9f878640ac649a6e6d87c7a8596c
i-syedali <49947224+i-syedali@users.noreply.github.com>
2020-09-08 18:32:12 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // load the http module
 var http = require('http');// configure our HTTP server
-var server = http.createServer(function (request, response) {response.writeHead(200, {"Content-Type": "text/plain"}); response.end("Hello Syed Ali !! \n")
+var server = http.createServer(functiion (request, response) {response.writeHead(200, {"Content-Type": "text/plain"}); response.end("Hello Syed Ali !! \n")
 });// listen on localhost:8000
 server.listen(8000);
 console.log("Server listening on port 8000 :  http://127.0.0.1:8000/");
