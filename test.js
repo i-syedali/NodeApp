467f1ab2f355f297bdf97ddab6ed7c0e44c49dd9
i-syedali <49947224+i-syedali@users.noreply.github.com>
2020-09-13 13:04:39 +0300

    Update test.js

--- test.js
+++ test.js
@@ -5,3 +5,20 @@ function test() {
 }
 
 if (module == require.main) require('test').run(test);
+
+var assert = require('assert')
+
+function test() {
+  assert.equal(2 + 2, 4);
+}
+
+if (module == require.main) require('test').run(test);
+
+var assert = require('assert')
+
+function test() {
+  assert.equal(2 + 2, 4);
+}
+
+if (module == require.main) require('test').run(test);
+
